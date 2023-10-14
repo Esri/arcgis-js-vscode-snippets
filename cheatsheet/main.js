7da7d25e83238017ea9e70971fc4cc07073c48cd
Estela Extremera <estelaextremeracampos@gmail.com>
2023-10-14 20:23:01 +0200

    show output datas when select a snippets

--- cheatsheet/main.js
+++ cheatsheet/main.js
@@ -1,13 +1,15 @@
 import "./style.css";
-import * as HtmlJson from "../snippets/html.json";
-import * as JavascriptJson from "../snippets/javascript.json";
-import * as JsonJson from "../snippets/json.json";
-import * as ScssJson from "../snippets/scss.json";
-import * as TypescriptJson from "../snippets/typescript.json";
-import * as TypescriptReactJson from "../snippets/typescriptreact.json";
+import HtmlJson from "../snippets/html.json";
+import JavascriptJson from "../snippets/javascript.json";
+import JsonJson from "../snippets/json.json";
+import ScssJson from "../snippets/scss.json";
+import TypescriptJson from "../snippets/typescript.json";
+import TypescriptReactJson from "../snippets/typescriptreact.json";
 
 const languageSelect = document.getElementById("language-select");
+let file;
 const allLanguages = [
+  { value: "", label: "Select a language" },
   { value: "HtmlJson", label: "Html" },
   { value: "JavascriptJson", label: "Javascript" },
   { value: "JsonJson", label: "Json" },
@@ -18,8 +20,12 @@ const allLanguages = [
 for (let i = 0; i < allLanguages.length; i++) {
   const option = document.createElement("option");
   option.setAttribute("value", allLanguages[i].value);
+  if (!allLanguages[i].value) {
+    option.setAttribute("selected", true)
+    option.setAttribute("disabled", true);
+    
+  }
   option.innerHTML = allLanguages[i].label;
-
   languageSelect.appendChild(option);
 }
 
@@ -37,25 +43,37 @@ const getFileBylanguageSelect = (languageSelected) => {
 };
 
 const createOptionsSelectSnippet = (keyFiles) => {
-  for (let i = 0; i < keyFiles.length; i++){
-    const option = document.createElement("option")
-    option.setAttribute("value", keyFiles[i])
-    option.innerHTML = keyFiles[i]
-    generalSelector.appendChild(option)
+  for (let i = 0; i < keyFiles.length; i++) {
+    const option = document.createElement("option");
+    option.setAttribute("value", keyFiles[i]);
+    option.innerHTML = keyFiles[i];
+    generalSelector.appendChild(option);
   }
-}
-const onChangelanguageSelect = (e) => {
-   while (generalSelector.firstChild) {
-     generalSelector.removeChild(generalSelector.firstChild);
-   }
-  const languageSelected = e.target.value
-  const file = getFileBylanguageSelect(languageSelected);
-  const keyfiles = Object.keys(file.default);
-  createOptionsSelectSnippet(keyfiles)
-  }
+};
 
+const outputContainer = document.getElementById("output")
+const outputElementBody = document.getElementById("output-body");
+const outputElementDescription = document.getElementById("output-description");
+const outputElementPrefix = document.getElementById("output-prefix");
+
+const onChangeGeneralSelector = (selectedValue) => {
+  const selectedData = file[selectedValue];
+
+  outputElementPrefix.textContent = `${selectedData.prefix}`;
+  outputElementBody.textContent = `${selectedData.body}`;
+  outputElementDescription.textContent = `${selectedData.description}`;
+};
+const onChangelanguageSelect = (e) => {
+  while (generalSelector.firstChild) {
+    generalSelector.removeChild(generalSelector.firstChild);
+  }
+  const languageSelected = e.target.value;
+  file = getFileBylanguageSelect(languageSelected);
+  const keyfiles = Object.keys(file);
+  createOptionsSelectSnippet(keyfiles);
+  onChangeGeneralSelector(keyfiles[0]);
+  outputContainer.setAttribute("class","output")
+};
 
 languageSelect.addEventListener("change", onChangelanguageSelect);
-
-
-
+generalSelector.addEventListener("change",(e)=> onChangeGeneralSelector(e.target.value));
