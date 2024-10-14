1be42fc33ddf7cc8fccbba523e73a9bf89efd8b1
unknown <bochaberidaisy22@gmail.com>
2024-10-14 17:23:16 +0300

    feat: Enhance Snippet Builder #46

--- builder/main.js
+++ builder/main.js
@@ -1,6 +1,7 @@
 let snippet = {
   name: "",
   prefix: "",
+  scope: [],
   desc: "",
   body: "",
   parse: (body) => {
@@ -26,11 +27,36 @@ ${x}
   toString: () => {
     return `"${snippet.name}": {
 "prefix": "${snippet.prefix}",
+"scope": "${Array.isArray(snippet.scope) ? snippet.scope.join(', ') : ''}",
 "body": ${snippet.parse(snippet.body)},
 "description": "${snippet.desc}"
 }`;
   },
 };
+
+document.querySelectorAll('#language-selection input[type="checkbox"]').forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    if (!Array.isArray(snippet.scope)) {
+      snippet.scope = [];
+    }
+
+    // If the checkbox is checked, add the value to the scope array
+    if (this.checked) {
+      if (!snippet.scope.includes(this.value)) {
+        snippet.scope.push(this.value);
+      }
+    } else {
+      // If unchecked, remove the value from the scope array
+      const index = snippet.scope.indexOf(this.value);
+      if (index > -1) {
+        snippet.scope.splice(index, 1);
+      }
+    }
+
+    // Render the snippet to reflect changes
+    renderSnippet();
+  });
+});
 
 function renderSnippet() {
   document.getElementById("snippets").innerText = snippet.toString();
@@ -58,10 +84,52 @@ Code:
 ${snippet.toString()}
 \`\`\`
 `);
+
   document.querySelector(
     "calcite-button"
   ).href = `${repoUrl}/issues/new?title=${title}&body=${body}`;
 }
+
+document.getElementById("import-snippet").addEventListener("change", (event) => {
+  const file = event.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      try {
+        const fileContent = e.target.result;
+        const snippetObject = JSON.parse(fileContent);
+
+        const snippetDisplay = document.getElementById("snippets");
+        snippetDisplay.innerText = JSON.stringify(snippetObject, null, 2);
+
+        hljs.highlightElement(snippetDisplay);
+
+      } catch (error) {
+        console.error("Failed to parse the snippet JSON: ", error);
+        document.getElementById("snippets").innerText = "Error parsing JSON. Please upload a valid snippet.";
+      }
+    };
+
+    reader.readAsText(file);
+  }
+});
+
+
+const copyButton = document.getElementById("copy-button");
+
+copyButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const snippetText = snippet.toString();
+  navigator.clipboard.writeText(snippetText)
+    .then(() => {
+      copyButton.textContent = "Copied";
+    })
+    setTimeout(() => {
+      copyButton.textContent = "Copy";
+    }, 1000);    
+});
 
 document.querySelectorAll("calcite-input").forEach((item) => {
   item.addEventListener("keyup", (evt) => {
